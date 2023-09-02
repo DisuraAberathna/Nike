fcc75843e6930a61ad467694cd9ef58767345767
Disura Chathwara <disura2005@gmail.com>
2023-09-02 09:24:48 +0530

    Initial commit

---
+++ src/sections/PopulerProducts.jsx
@@ -0,0 +1,26 @@
+import { PopulerProductCard } from "../components/PopulerProductCard";
+import { products } from "../constants";
+
+const PopulerProducts = () => {
+  return (
+    <section id="products" className="max-container  max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">
+          Our <span className="text-coral-red">Popular</span> Products
+        </h2>
+        <p className="lg:max-w-lg mt-2 text-slate-gray">
+          Experience top-notch quality abd style with our sought-after
+          selections. Discover a world of comfort, design, and value.
+        </p>
+      </div>
+
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopulerProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default PopulerProducts;
